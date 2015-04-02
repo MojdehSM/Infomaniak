ac7b0e21265e410f16612719430a6c09808a2326
MojdehSM <mohamadi.mojde@yahoo.com>
2015-04-02 16:44:48 +0200

    First commit

---
+++ Infomaniak/src/main/java/com/infomaniak/entities/Compus.java
@@ -0,0 +1,5 @@
+package com.infomaniak.entities;
+
+public class Compus {
+
+}
---
+++ Infomaniak/src/main/java/com/infomaniak/main/InfoMain.java
@@ -0,0 +1,10 @@
+package com.infomaniak.main;
+
+public class InfoMain {
+	
+	public static void main(String[] args) {
+	
+			System.out.println("Hello word");
+	}
+
+}
