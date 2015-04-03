fe356071c32a9bbc46dead3b107bb9448ef76e12
MojdehSM <mohamadi.mojde@yahoo.com>
2015-04-03 09:34:43 +0200

    Package Exception

---
+++ Infomaniak/src/main/java/com/infomaniak/exceptions/FullCampusException.java
@@ -0,0 +1,13 @@
+package com.infomaniak.exceptions;
+
+public class FullCampusException extends RuntimeException {
+
+	public FullCampusException() {
+		super();
+	}
+
+	public FullCampusException(String s) {
+		super(s);
+	}
+
+}
